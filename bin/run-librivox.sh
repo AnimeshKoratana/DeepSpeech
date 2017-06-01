ddbbfaf695cc8448af0164d7f59132e9309a0d82
Animesh Koratana <animesh.koratana@gmail.com>
2017-06-01 17:55:50 -0400

    Update run-librivox.sh

--- bin/run-librivox.sh
+++ bin/run-librivox.sh
@@ -31,7 +31,7 @@ python -u DeepSpeech.py \
   --dev_batch_size 12 \
   --test_batch_size 12 \
   --learning_rate 0.0001 \
-  --epoch 15 \
+  --epoch 20 \
   --display_step 5 \
   --validation_step 5 \
   --dropout_rate 0.30 \
